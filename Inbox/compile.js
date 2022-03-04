a6b38d25f74143dc5cfaa806c36f5923ff032395
xinlang <fengxinlang@126.com>
2022-03-04 15:40:55 +0800

    first commit

---
+++ Inbox/compile.js
@@ -0,0 +1,8 @@
+const path = require('path');  // 相比把路径写死，容易不同平台迁移
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];   
---
+++ Inbox/test/Inbox.test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //首字母大写，因为Web3是个struct，要用来构造新的实例
+
+const web3 = new Web3('');
